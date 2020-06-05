51eaad69292b3d18335fe4301d823d6bf8fa3498
diego_neri <diego@diegoneri.com>
2020-06-05 20:44:25 -0300

    Finaliza o exercício CalculadoraIdade

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculadoraIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataAtual = DateTime.Now;
+            DateTime diaDoMeuNascimento = new DateTime(1986, 11, 05, 16, 20, 00);
+
+            TimeSpan intervaloVivido = dataAtual - diaDoMeuNascimento;
+
+            string dataCurta = diaDoMeuNascimento.ToShortDateString();
+            int intervaloEmDias = intervaloVivido.Days;
+
+            Console.WriteLine($"Eu nasci em {dataCurta} e já vivi {intervaloEmDias} dias");
+
+        }
+    }
+}
